f47d511c0940e3eb12b76b6f6714194fdb28378a
ASUS-EMPEROR\Bart <bart.vanraes@gmail.com>
2020-03-06 22:02:59 +0100

    IndexControllerTest added

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.controllers;
+
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+    private IndexController indexController;
+
+    @Mock
+    private RecipeService recipeService;
+
+    @Mock
+    private Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        String returnValue = indexController.getIndexPage(model);
+
+        assertEquals(returnValue, "index");
+        verify(model, times(1)).addAttribute("recipes", recipesData);
+        verify(recipeService, times(1)).getRecipes();
+    }
+}
